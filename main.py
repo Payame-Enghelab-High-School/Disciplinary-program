83553afbe5d98a97637548f13afe470e4b728990
sys-09 <payameenghelab7@gmail.com>
2023-11-08 10:27:59 +0330

    Add Searchbar part

---
+++ main.py
@@ -0,0 +1,14 @@
+from tkinter import Tk, Entry, Button, Label
+root = Tk()
+
+Label(root, text="Search:", bg='white', fg='black', font=('', 10)).place(x=4.5, y=8)
+Search_bar = Entry(root, background="#ffe", width=100)
+Search_bar.place(x=80, y=10)
+Search_btn = Button(root, text="Go!", width=10, height=1, bg="white", fg="black")
+Search_btn.place(x=700, y=7)
+
+root.title("Disciplinary Program")
+root.resizable(False, False)
+root.geometry("800x500")
+root.config(bg="#fff")
+root.mainloop()
